60b6803be17b595a204878dc4ac14bee56d06a0c
Andy-Roger <andyrogerkats@gmail.com>
2018-01-20 20:30:34 -0800

    re-arranged the folders

---
+++ Assets/MDM/maze/mazeMap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mazeMap : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        
+    }
+
+    private void Update()
+    {
+        //transform.Rotate(0,1,0);
+    }
+}
---
+++ Assets/MDM/maze/mazeMapController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mazeMapController : MonoBehaviour
+{
+
+    public GameObject mazeMap;
+
+    private void Start()
+    {
+        inputManager.trackedController1.MenuButtonClicked += menuButton1Clicked;
+        inputManager.trackedController2.MenuButtonClicked += menuButton2Clicked;
+        inputManager.trackedController1.MenuButtonUnclicked += menuButton1Unclicked;
+        inputManager.trackedController2.MenuButtonUnclicked += menuButton2Unclicked;
+    }
+
+    private void OnApplicationQuit()
+    {
+        inputManager.trackedController1.MenuButtonClicked -= menuButton1Clicked;
+        inputManager.trackedController2.MenuButtonClicked -= menuButton2Clicked;
+        inputManager.trackedController1.MenuButtonUnclicked -= menuButton1Unclicked;
+        inputManager.trackedController2.MenuButtonUnclicked -= menuButton2Unclicked;
+    }
+
+    void menuButton1Clicked(object sender, ClickedEventArgs e)
+    {
+        mazeMap.transform.parent = inputManager.trackedController1.transform;
+        mazeMap.transform.localPosition = Vector3.up;
+        mazeMap.SetActive(true);
+    }
+
+    void menuButton2Clicked(object sender, ClickedEventArgs e)
+    {
+        mazeMap.transform.parent = inputManager.trackedController2.transform;
+        mazeMap.transform.localPosition = Vector3.up;
+        mazeMap.SetActive(true);
+    }
+
+    void menuButton1Unclicked(object sender, ClickedEventArgs e)
+    {
+        mazeMap.transform.parent = null;
+        mazeMap.SetActive(false);
+    }
+
+    void menuButton2Unclicked(object sender, ClickedEventArgs e)
+    {
+        mazeMap.transform.parent = null;
+        mazeMap.SetActive(false);
+    }
+}
